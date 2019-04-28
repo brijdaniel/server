a575832286fa8046ab836d5babf9b49d520fc502
Brij Daniel <brij.daniel@gmail.com>
2019-04-28 12:07:30 +0100

    Facelift :)

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 
 # Connect to redis server
 from app import redis
