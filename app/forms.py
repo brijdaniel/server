aa42509b5e6ded409ed7e226093f063aec1e7bd8
Brij Daniel <brij.daniel@gmail.com>
2019-05-08 09:12:07 +0100

    Form auto fill fixed

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, DecimalField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
-from app import redis
 
 class SpTimer(FlaskForm):
-	timer = DecimalField('timer', validators=[DataRequired()], places=2, default=float(redis.db.get('pihouse/sprinkler/timer')))
+	timer = DecimalField('timer', validators=[DataRequired()], places=2)
 	submit = SubmitField('Set Timer')
 
 class SpScheduleForm(FlaskForm):
-	schedule = redis.db.get('pihouse/sprinkler/schedule').split()
-	minute = StringField('minute', validators=[DataRequired()], default=schedule[0])
-	hour = StringField('hour', validators=[DataRequired()], default=schedule[1])
-	dom = StringField('dom', validators=[DataRequired()], default=schedule[2])
-	month = StringField('month', validators=[DataRequired()], default=schedule[3])
-	dow = StringField('dow', validators=[DataRequired()], default=schedule[4])
+	minute = StringField('minute', validators=[DataRequired()])
+	hour = StringField('hour', validators=[DataRequired()])
+	dom = StringField('dom', validators=[DataRequired()])
+	month = StringField('month', validators=[DataRequired()])
+	dow = StringField('dow', validators=[DataRequired()])
 	use_schedule = BooleanField('Use Schedule', default='checked')
 	submit = SubmitField('Update Schedule')
--- app/routes.py
+++ app/routes.py
@@ -61,14 +61,13 @@ def action(action):
 @register_breadcrumb(app, '.sprinkler.timer', 'Timer')
 def sp_timer():
 
-	form = SpTimer()
+	form = SpTimer(timer=float(redis.db.get('pihouse/sprinkler/timer')))
 	if request.method == 'GET' or not form.validate_on_submit():
 		if not isinstance(redis.db.get('pihouse/sprinkler/timer'), int): 
 			mqtt.client.publish('pihouse/sprinkler/timer', "request")
 			sleep(0.1)
 		if not str(redis.db.get('pihouse/sprinkler/timer')) == "request":
 			flash('Timer is currently %s mins' %float(redis.db.get('pihouse/sprinkler/timer')))
-
 	elif request.method == 'POST' and form.validate_on_submit():
 		flash('Timer set to %s mins' %form.timer.data)
 		mqtt.client.publish('pihouse/sprinkler/timer', float(form.timer.data))
@@ -78,19 +77,18 @@ def sp_timer():
 @app.route('/sprinkler/schedule', methods=['GET', 'POST'])
 @register_breadcrumb(app, '.sprinkler.schedule', 'Schedule')
 def sp_schedule():
+	schedule = redis.db.get('pihouse/sprinkler/schedule').split()
+	form = SpScheduleForm(minute=schedule[0], hour=schedule[1], dom=schedule[2], month=schedule[3], dow=schedule[4])
 	
-	form = SpScheduleForm()
 	if request.method == 'GET' or not form.validate_on_submit():
 		if not isinstance(redis.db.get('pihouse/sprinkler/schedule'), str): # or list?
 			mqtt.client.publish('pihouse/sprinkler/schedule', "request")
 			sleep(0.1)
 			# read schedule from db and pass as default field values
-
 	elif request.method == 'POST' and form.validate_on_submit():
 		schedule = '%s %s %s %s %s' %(str(form.minute.data), str(form.hour.data), str(form.dom.data), str(form.month.data), str(form.dow.data))
 		mqtt.client.publish('pihouse/sprinkler/schedule', str(schedule))
 		mqtt.client.publish('pihouse/sprinkler/schedule/set', str(form.use_schedule.data))
-		
 		flash('Schedule set!')
 		#return redirect(url_for('sp_schedule'))
 
