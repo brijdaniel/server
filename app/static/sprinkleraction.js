a9950646ccaff55279e0552f01d50865cc8c2959
Brij Daniel <brij.daniel@gmail.com>
2019-05-09 13:02:53 +0100

    js status returning false

--- app/static/sprinkleraction.js
+++
@@ -1,8 +0,0 @@
-function action(action){
-	$.get('sprinkler/' + action).done(function(response){
-		// want to repeat the jinja2 if statement span id=if
-		// not sure how to do this
-	}).fail(function(){
-		// error sprinkler not respond
-	})
-}
